591d55d4ebbb66fc9d23253bf9d492456033474a
David Seddon <david@pepperpotdesign.co.uk>
2012-11-12 17:11:38 +0000

    Added packaging files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name="Django DateInYear",
+    version="0.1",
+    description="Django model and form fields for storing dates without years",
+    author="David Seddon",
+    author_email="david@pepperpotdesign.co.uk",
+    url="http://www.pepperpotdesign.co.uk/s",
+    py_modules=['models', 'forms', 'helpers'],
+    download_url="http://github.com/seddonym/djangodateinyear/")
