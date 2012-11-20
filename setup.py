bf5d0771a7bfbd3503de400d03d64764cabe2a2f
David Seddon <david@pepperpotdesign.co.uk>
2012-11-20 14:51:03 +0000

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "django-yearlessdate",
-    version = "0.1-beta",
+    version = "0.2-beta",
     packages = ['djangoyearlessdate'],
     author = "David Seddon",
     author_email = "david@pepperpotdesign.co.uk",
