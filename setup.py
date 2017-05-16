af1d4e2866323592b4dd872ab38aab76cc83d70a
Roumen Antonov <github@roumen.us>
2017-05-16 16:07:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "django-yearlessdate",
-    version = "0.3-beta",
+    version = "0.4-beta",
     packages = ['djangoyearlessdate'],
     author = "David Seddon",
     author_email = "david@pepperpotdesign.co.uk",
